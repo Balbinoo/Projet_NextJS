64264c17d4de58c8ee0c91691982f630a40fb9c2
Rodrigo Balbino <rodrigo080599@hotmail.com>
2025-02-07 20:50:04 +0100

    Add freeToplay model data

---
+++ lib/modals/FreeToGame.ts
@@ -0,0 +1,22 @@
+import { Schema, models, model } from "mongoose";
+
+const GameSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    thumbnail: { type: String, required: true },
+    short_description: { type: String, required: true },
+    game_url: { type: String, required: true },
+    genre: { type: String, required: true },
+    platform: { type: String, required: true },
+    publisher: { type: String, required: true },
+    developer: { type: String, required: true },
+    release_date: { type: String, required: true }, // Keeping as string to match API
+    freetogame_profile_url: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }, // Auto-generated timestamp
+  },
+  { timestamps: true }
+);
+
+const Game = models.Game || model("Game", GameSchema);
+
+export default Game;
